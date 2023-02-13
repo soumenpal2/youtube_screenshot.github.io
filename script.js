25e4373b2a72cfc1d671ed0ee6883955be25bcd1
soumen pal <soumenpal.07557@gmail.com>
2023-02-13 08:09:16 +0530

    youtube screenshot

---
+++ script.js
@@ -0,0 +1,6 @@
+let subscribe=document.getElementById("subscribe");
+subscribe.addEventListener("click",()=>{
+    let list= document.createElement("div");
+    list.className="list";
+    listcontainer.appendchild(list);
+});
